87400a878460b82074e192dd16817b904eea1431
Marcin Szyszka <marcin.szyszka@abcpro.pl>
2017-08-11 14:49:06 +0200

    Added page object model classes

---
+++ Selenium.BrowserTestsExample/POM/ResultsPage.cs
@@ -0,0 +1,15 @@
+﻿using Bumblebee.Implementation;
+using Bumblebee.Interfaces;
+using Bumblebee.Setup;
+using OpenQA.Selenium;
+
+namespace Selenium.BrowserTestsExample.POM {
+    public class ResultsPage : WebBlock {
+        public ResultsPage(Session session) : base(session) {
+        }
+
+        public ITextField<ResultsPage> FirstResultAddress {
+            get { return new TextField<ResultsPage>(this, By.XPath(@"//*[@id=""rso""]/div[2]/div/div[1]/div/div/div/div/div[1]/cite")); }
+        }
+    }
+}
---
+++ Selenium.BrowserTestsExample/POM/SearchPage.cs
@@ -0,0 +1,24 @@
+﻿using Bumblebee.Implementation;
+using Bumblebee.Interfaces;
+using Bumblebee.Setup;
+using OpenQA.Selenium;
+
+namespace Selenium.BrowserTestsExample.POM {
+    public class SearchPage : WebBlock {
+        public SearchPage(Session session) : base(session) {
+
+        }
+
+        public ITextField<SearchPage> SearchInput {
+            get { return new TextField<SearchPage>(this, By.XPath(@"//*[@id=""lst-ib""]")); }
+        }
+
+        public IClickable<SearchPage> SearchButton {
+            get { return new Clickable<SearchPage>(this, By.XPath(@"//*[@id=""tsf""]/div[2]/div[3]/center/input[1]")); }
+        }
+
+        public ITextField<ResultsPage> FirstResultAddress {
+            get { return new ResultsPage(base.Session).FirstResultAddress; }
+        }
+    }
+}
